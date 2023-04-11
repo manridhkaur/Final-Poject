a007051e14f2cacbd117dc3fad1154b86231f592
manridhkaur <122409269+manridhkaur@users.noreply.github.com>
2023-04-11 14:33:25 -0400

    Add files via upload

---
+++ Final Product HTML/javascript/script.js
@@ -0,0 +1,14 @@
+let previousScrollPosition = 0;
+
+const isScrollingDown = () => {
+  let currentScrolledPosition = window.scrollY || window.pageYOffset;
+  let scrollingDown;
+
+  if (currentScrolledPosition > previousScrollPosition) {
+    scrollingDown = true;
+  } else {
+    scrollingDown = false;
+  }
+  previousScrollPosition = currentScrolledPosition;
+  return scrollingDown;
+}
